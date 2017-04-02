b6e93d5675944fa9c19544ee86d4ab4a4d52eb5d
Moti Zilberman <moti@digital-detox.co.uk>
2017-04-02 11:20:40 +0100

    chore(index.js): remove unused code

--- src/index.js
+++ src/index.js
@@ -3,15 +3,6 @@
 import type { NodePath, Scope } from 'babel-traverse';
 import syntaxDynamicImport from 'babel-plugin-syntax-dynamic-import';
 
-function deepestScopeOf (path: NodePath, scopes: Scope[]): ?Scope {
-  let scope = path.scope;
-  do {
-    if (scopes.indexOf(scope) !== -1) {
-      return scope;
-    }
-  } while (scope = scope.parent); // eslint-disable-line no-cond-assign
-}
-
 type BabelVisitors = {
   [key: string]: (path: NodePath) => void
 };
